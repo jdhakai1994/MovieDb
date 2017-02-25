4e26038f5589094db376ded4c5c393d24a80c20d
Jayabrata Dhakai <jayabratadhakai@gmail.com>
2017-02-25 21:31:11 +0530

    Added the API KEY

---
+++ app/src/main/java/com/example/android/moviedb/utilities/GlobalConstants.java
@@ -0,0 +1,11 @@
+package com.example.android.moviedb.utilities;
+
+/**
+ * Created by Jayabrata Dhakai on 2/25/2017.
+ */
+
+public class GlobalConstants {
+
+    public static String API_KEY = "0714b520af4b472889d9b6ce22e54173";
+
+}
