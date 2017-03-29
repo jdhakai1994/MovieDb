1bec834bbd620d0254d4c2412180d7702023ebb0
Jayabrata Dhakai <jayabratadhakai@gmail.com>
2017-03-29 20:45:26 +0530

    Implement detail layout with constraint layout.

--- app/src/main/java/com/example/android/moviedb/DetailActivity.java
+++ app/src/main/java/com/example/android/moviedb/DetailActivity.java
@@ -2,12 +2,9 @@ package com.example.android.moviedb;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.android.moviedb.models.Results;
 import com.example.android.moviedb.utilities.QueryUtils;
@@ -17,14 +14,8 @@ public class DetailActivity extends Activity {
 
     private Results mMovie;
 
-    private ProgressBar mProgressBarBackDrop;
-    private ProgressBar mProgressBarPoster;
-
     private ImageView mBackdrop;
     private ImageView mPoster;
-
-    private ImageView mBackdropErrorImage;
-    private ImageView mPosterErrorImage;
 
     private TextView mTitle;
     private TextView mDetailTitle;
@@ -49,55 +40,19 @@ public class DetailActivity extends Activity {
      * Helper Method to set up the UI elements
      */
     private void hookUpDetailUI(){
-        mProgressBarBackDrop = (ProgressBar) findViewById(R.id.pb_backdrop_image);
         mBackdrop = (ImageView) findViewById(R.id.iv_backdrop);
-        mBackdropErrorImage = (ImageView) findViewById(R.id.iv_backdrop_error_image);
         String backdropImageUrl = QueryUtils.getBackdropImageUrl(mMovie.getBackdropPath());
         Picasso.with(this)
                 .load(backdropImageUrl)
-                .into(mBackdrop, new com.squareup.picasso.Callback() {
-                    @Override
-                    public void onSuccess() {
-                        if (mProgressBarBackDrop != null) {
-                            mProgressBarBackDrop.setVisibility(View.GONE);
-                            mBackdrop.setVisibility(View.VISIBLE);
-                        }
-                    }
+                .error(R.drawable.image_error)
+                .into(mBackdrop);
 
-                    @Override
-                    public void onError() {
-                        if (mProgressBarBackDrop != null) {
-                            mProgressBarBackDrop.setVisibility(View.GONE);
-                            mBackdropErrorImage.setVisibility(View.VISIBLE);
-                        }
-                        Toast.makeText(getApplicationContext(), R.string.error_loading_image_toast, Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-        mProgressBarPoster = (ProgressBar) findViewById(R.id.pb_poster_image);
         mPoster = (ImageView) findViewById(R.id.iv_poster);
-        mPosterErrorImage = (ImageView) findViewById(R.id.iv_poster_error_image);
         String posterImageUrl = QueryUtils.getPosterImageUrlDetail(mMovie.getPosterPath());
         Picasso.with(this)
                 .load(posterImageUrl)
-                .into(mPoster, new com.squareup.picasso.Callback() {
-                    @Override
-                    public void onSuccess() {
-                        if (mProgressBarPoster != null) {
-                            mProgressBarPoster.setVisibility(View.GONE);
-                            mPoster.setVisibility(View.VISIBLE);
-                        }
-                    }
-
-                    @Override
-                    public void onError() {
-                        if (mProgressBarPoster != null) {
-                            mProgressBarPoster.setVisibility(View.GONE);
-                            mPosterErrorImage.setVisibility(View.VISIBLE);
-                        }
-                        Toast.makeText(getApplicationContext(), R.string.error_loading_image_toast, Toast.LENGTH_SHORT).show();
-                    }
-                });
+                .error(R.drawable.image_error)
+                .into(mPoster);
 
         mTitle = (TextView) findViewById(R.id.tv_title);
         mTitle.setText(mMovie.getTitle());
